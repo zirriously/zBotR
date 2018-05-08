7e29468a1df2123a3f08c37f3dd7f4316abe305d
zirr <chri650t@edu.easj.dk>
2018-05-08 18:19:25 +0200

    Moved user handling to own class

--- zBotR/Program.cs
+++ zBotR/Program.cs
@@ -17,8 +17,9 @@ namespace zBotR
         private DiscordSocketClient _client;
         private string _twitchclientid = "";
         private TwitchLookup _twitchLookup;
+        private UserHandler _userHandler;
         private List<string> _optout;
-        private const string ApiLink = "https://api.twitch.tv/kraken/streams/";
+        public const string ApiLink = "https://api.twitch.tv/kraken/streams/";
         private ulong _liveRoleId;
         private IRole _liveRole;
         private string _token;
@@ -36,6 +37,7 @@ namespace zBotR
             _client.Log += Log;
             _client.MessageReceived += MessageReceived;
 
+            //json to vars
             var botvars = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(@".\botvars.json"));
             _twitchclientid = botvars.twitchclientid;
             string[] optoutarray = botvars.optout.ToObject<string[]>();
@@ -43,6 +45,7 @@ namespace zBotR
             _liveRoleId = botvars.roleid;
             _token =  botvars.token;
             _twitchLookup = new TwitchLookup(_twitchclientid);
+
 
             await _client.LoginAsync(TokenType.Bot, _token);
             await _client.StartAsync();
@@ -58,104 +61,15 @@ namespace zBotR
                 Console.ResetColor();
                 CheckUsersTimer();
 
-                foreach (var guild in _client.Guilds)
+                foreach (var guild in _client.Guilds) // weird way of getting role. Should fix
                 {
                     _liveRole = guild.GetRole(_liveRoleId);
                 }
 
+                _userHandler = new UserHandler(_client, _liveRole, _twitchLookup);
                 return Task.CompletedTask;
             };
             await Task.Delay(Timeout.Infinite);
-        }
-
-        private async Task CheckUsers()
-        {
-            await Task.Run(async () =>
-            {
-                foreach (var guild in _client.Guilds)
-                {
-                    foreach (var user in guild.Users)
-                    {
-                        // check if streaming
-                        if (user.Activity != null && user.Activity.Type == ActivityType.Streaming
-                            && !_optout.Contains(user.Id.ToString()))
-                        {
-                            CheckSingleUser(user);
-                        }
-
-                        // check if not streaming but has role
-                        else if (user.Activity != null &&
-                                 (user.Roles.Contains(_liveRole) && user.Activity.Type != ActivityType.Streaming))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            await Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is no longer streaming. Removing role."));
-                            Console.ResetColor();
-                            await user.RemoveRoleAsync(_liveRole);
-                        }
-                        else if (user.Activity == null && user.Roles.Contains(_liveRole))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            await Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is no longer streaming. Removing role."));
-                            Console.ResetColor();
-                            await user.RemoveRoleAsync(_liveRole);
-                        }
-                    }
-                }
-
-                return Task.CompletedTask;
-            });
-        }
-
-        private async Task CheckSingleUser(SocketGuildUser user)
-        {
-            StreamingGame streamingGame;
-            if (user.Activity != null && user.Activity.Type == ActivityType.Streaming)
-            {
-                streamingGame = (StreamingGame) user.Activity;
-            }
-            else
-            {
-                await Log(new LogMessage(LogSeverity.Info, "CRITICAL", "Error"));
-                return;
-            }
-            var twitchUserName = streamingGame.Url.Substring(streamingGame.Url.LastIndexOf('/') + 1);
-            var apiRequest = ApiLink + twitchUserName;
-            var apiRequestResponse = await _twitchLookup.TwitchRequest(apiRequest);
-
-            if (apiRequestResponse == null)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                await Log(new LogMessage(LogSeverity.Info, "API", "Error fetching API data."));
-                Console.ResetColor();
-                return;
-            }
-
-            var responseJson = JsonConvert.DeserializeObject<dynamic>(apiRequestResponse);
-            var gameStreamed = responseJson.stream.game;
-
-            await Log(new LogMessage(LogSeverity.Info, "API", $"{user} is streaming {gameStreamed}"));
-
-            if (gameStreamed == "Factorio" && !user.Roles.Contains(_liveRole))
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                await Log(new LogMessage(LogSeverity.Info, "API", $"{user} is streaming Factorio and does not have live role. Assigning!"));
-                Console.ResetColor();
-                await user.AddRoleAsync(_liveRole);
-            }
-            else if (gameStreamed == "Factorio" && user.Roles.Contains(_liveRole))
-            {
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                await Log(new LogMessage(LogSeverity.Info, "API",
-                    $"{user} is streaming Factorio, but already has role. Not assigning."));
-                Console.ResetColor();
-            }
-            else if (gameStreamed != "Factorio" && user.Roles.Contains(_liveRole))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                await Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is streaming, but not Factorio. Removing role."));
-                Console.ResetColor();
-                await user.RemoveRoleAsync(_liveRole);
-            }
         }
 
         private void CheckUsersTimer()
@@ -167,7 +81,7 @@ namespace zBotR
                     Console.ForegroundColor = ConsoleColor.Blue;
                     await Log(new LogMessage(LogSeverity.Info, "Timer", "Checking users for stream status..."));
                     Console.ResetColor();
-                    await CheckUsers();
+                    await _userHandler.CheckUsers(_optout);
                     Console.ForegroundColor = ConsoleColor.Blue;
                     await Log(new LogMessage(LogSeverity.Info, "Timer", "Done. Sleeping for 1 minute..."));
                     Console.ResetColor();
@@ -218,7 +132,7 @@ namespace zBotR
             }
         }
 
-        private Task Log(LogMessage message)
+        public static Task Log(LogMessage message)
         {
             Console.WriteLine(message.ToString());
             return Task.CompletedTask;
---
+++ zBotR/UserHandler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace zBotR
+{
+    public class UserHandler
+    {
+        private DiscordSocketClient _discordSocketClient;
+        private IRole _liveRole;
+        private TwitchLookup _twitchLookup;
+
+        public UserHandler(DiscordSocketClient discordSocketClient, IRole liveRole, TwitchLookup twitchLookup)
+        {
+            _discordSocketClient = discordSocketClient;
+            _liveRole = liveRole;
+            _twitchLookup = twitchLookup;
+        }
+
+        public async Task CheckUsers(List<string> optout)
+        {
+            await Task.Run(async () =>
+            {
+                foreach (var guild in _discordSocketClient.Guilds)
+                {
+                    foreach (var user in guild.Users)
+                    {
+                        // check if streaming
+                        if (user.Activity != null && user.Activity.Type == ActivityType.Streaming
+                                                  && !optout.Contains(user.Id.ToString()))
+                        {
+                            CheckSingleUser(user);
+                        }
+
+                        // check if not streaming but has role
+                        else if (user.Activity != null &&
+                                 (user.Roles.Contains(_liveRole) && user.Activity.Type != ActivityType.Streaming))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            await Program.Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is no longer streaming. Removing role."));
+                            Console.ResetColor();
+                            await user.RemoveRoleAsync(_liveRole);
+                        }
+                        else if (user.Activity == null && user.Roles.Contains(_liveRole))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            await Program.Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is no longer streaming. Removing role."));
+                            Console.ResetColor();
+                            await user.RemoveRoleAsync(_liveRole);
+                        }
+                    }
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+
+        private async Task CheckSingleUser(SocketGuildUser user)
+        {
+            StreamingGame streamingGame;
+            if (user.Activity != null && user.Activity.Type == ActivityType.Streaming)
+            {
+                streamingGame = (StreamingGame)user.Activity;
+            }
+            else
+            {
+                await Program.Log(new LogMessage(LogSeverity.Info, "CRITICAL", "Error"));
+                return;
+            }
+            var twitchUserName = streamingGame.Url.Substring(streamingGame.Url.LastIndexOf('/') + 1);
+            var apiRequest = Program.ApiLink + twitchUserName;
+            var apiRequestResponse = await _twitchLookup.TwitchRequest(apiRequest);
+
+            if (apiRequestResponse == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Program.Log(new LogMessage(LogSeverity.Info, "API", "Error fetching API data."));
+                Console.ResetColor();
+                return;
+            }
+
+            var responseJson = JsonConvert.DeserializeObject<dynamic>(apiRequestResponse);
+            var gameStreamed = responseJson.stream.game;
+
+            await Program.Log(new LogMessage(LogSeverity.Info, "API", $"{user} is streaming {gameStreamed}"));
+
+            if (gameStreamed == "Factorio" && !user.Roles.Contains(_liveRole))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                await Program.Log(new LogMessage(LogSeverity.Info, "API", $"{user} is streaming Factorio and does not have live role. Assigning!"));
+                Console.ResetColor();
+                await user.AddRoleAsync(_liveRole);
+            }
+            else if (gameStreamed == "Factorio" && user.Roles.Contains(_liveRole))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                await Program.Log(new LogMessage(LogSeverity.Info, "API",
+                    $"{user} is streaming Factorio, but already has role. Not assigning."));
+                Console.ResetColor();
+            }
+            else if (gameStreamed != "Factorio" && user.Roles.Contains(_liveRole))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                await Program.Log(new LogMessage(LogSeverity.Info, "Client", $"{user} is streaming, but not Factorio. Removing role."));
+                Console.ResetColor();
+                await user.RemoveRoleAsync(_liveRole);
+            }
+        }
+    }
+}
